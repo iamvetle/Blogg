175079adf40c9bd31a7b3af465f39ba5d1ef64be
iamvetle <vetle122@gmail.com>
2023-08-11 23:45:25 +0200

    first commit

---
+++ backend/api/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+# CustomUser
+
+# username
+# lastname
+# email
+# password
+# birthday
+
+# followers
+# follows
+
+# posts ->
+
+###
+
+# a post
+
+# title
+# content
+# published_date
+# last modified
+# author
+# tags
