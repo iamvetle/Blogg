36447b658f23f6195c962bc8b11a5dc4a96d7d9b
iamvetle <vetle122@gmail.com>
2023-08-12 17:56:16 +0200

    much closer to finishing user model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,29 +1,68 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, password):
-        pass
-    
-    def create_superuser(self, username, email, password):
-        pass
+    def create_user(self, username, email, password, first_name, last_name):
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-class CustomUser(AbstractBaseUser):
-    username = models.TextField(max_length=15, unique=True)
+    def create_superuser(self, username, email, password, first_name, last_name):
+        user = self.create_user((username, email, password, first_name, last_name)) # Calls the function/method above
+        user.is_admin=True
+        user.is_staff=True
+        user.is_superuser=True
+        user.save(using=self._db)
+        return user
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=15, unique=True)
     email = models.EmailField(unique=True)
-    password = models.
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = CustomUserManager
+
+    USERNAME_FIELD = "username"
+
+    REQUIRED_FIELDS = [
+        "first_name",
+        "last_name",
+        "email",
+    ]
+
+    def __str__(self):
+        return self.username
+    
+    def full_name(self):
+        f'{self.first_name} + {self.last_name}'
+
+    def short_name(self):
+        return self.first_name
+    
+    def e_mail(self):
+        return self.email
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     date_published = models.DateField(auto_created=True)
     last_modified = models.DateTimeField(auto_now=True)
-    pass
-    #pictures?
+    author = models.one_to_one_field(CustomUser)
+
 
 class Comments(models.Model):
+
     
 
 
