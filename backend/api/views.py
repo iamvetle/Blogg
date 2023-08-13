c26442edd5b3cde1c6e2e5a9e4355ecf0623d373
iamvetle <vetle122@gmail.com>
2023-08-13 16:02:38 +0200

    closer to getting 'userprofile' done

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,20 +1,33 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import UserSerializer
+from .serializers import UserSerializer, ProfileSerializer
 from rest_framework import status
+from .models import CustomUser, CustomUserManager
 
 class MyAccountView(APIView):
     
     def get(self, request):
         
-        if request.user.authenticated:
+        if request.user.is_authenticated:
             serializer = UserSerializer(request.user)
             return Response(serializer.data, status=status.HTTP_200_OK) # AUTHORIZED - OK
         else:
             return Response(status=status.HTTP_401_UNAUTHORIZED) # NOT AUTHORIZED - NOT OK
         
-        
+class UserProfileView(APIView):
+    
+    def get(self, request, id):
+        if request.user.is_authenticated:
+            try:
+                user = CustomUser.CustomUserManager.get(pk=id)
+            except CustomUser.DoesNotExist:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+            
+            serializer = ProfileSerializer(request.user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
     # if is authentication
     
     # def get userinformation
