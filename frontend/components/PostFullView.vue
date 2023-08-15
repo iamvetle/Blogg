5b75d73051837d9148a70576ec78848c6e06818a
iamvetle <vetle122@gmail.com>
2023-08-15 19:38:07 +0200

    .

---
+++ frontend/components/PostFullView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <button>Back</button>
+        <h2 class="text-3xl">{{ post.title }}</h2>
+        <p>{{ post.content }}</p>
+    </div>
+</template>
+
+<script setup>
+const prop = defineProps(["post"])
+</script>
---
+++ frontend/components/PostPartialView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div>{{ postDetail.title }}</div> by {{ postDetail.author.username }}
+        <div class="w-[200px] h-[200px] border">
+        {{ postDetail.content }}
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+const post = defineProps(["postDetail"])
+
+
+</script>
