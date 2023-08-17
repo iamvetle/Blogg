0738b1beefac852043fd143b398118569726b59b
iamvetle <vetle122@gmail.com>
2023-08-18 01:17:14 +0200

    .

---
+++ frontend/assets/fxcode.vue
@@ -0,0 +1,173 @@
+<template>
+    <main
+      class="blog-page bg-neutral-100 pt-[undefinedpx] pr-[undefinedpx] pb-[undefinedpx] pl-[undefinedpx] flex flex-col overflow-hidden relative"
+    >
+      <section class="grid relative min-h-[387px]">
+        <img
+          class="w-[calc(100%_-_1px)] object-cover absolute left-0 right-px top-0 bottom-[86px] min-h-[301px]"
+          src="/assets/4c15b3c271b971bee165d968ab15ae42.png"
+          alt="alt text"
+        />
+        <div
+          class="bg-[rgb(0,128,117)] w-full h-[63px] flex justify-end items-center grow relative min-h-[63px] mt-[301px] mr-px mb-[23px]"
+        >
+          <div class="basis-[894px]"></div>
+          <div class="grid min-w-[335px]">
+            <div
+              class="xxl-xxxl:w-full xxl-xxxl:mt-[21px] xxl-xxxl:mr-0 xxl-xxxl:mb-[21px] xxl-xxxl:ml-auto xxl-xxxl:aspect-[auto] flex items-center grow relative my-[21px]"
+            >
+              <div
+                class="flex justify-end font-normal text-[14px] leading-normal font-Roboto text-white text-right tracking-[0.04px] shrink-0 basis-[50px]"
+              >
+                Contact
+              </div>
+              <div class="basis-10"></div>
+              <div
+                class="flex justify-end font-normal text-[14px] leading-normal font-Roboto text-white text-right tracking-[0.04px] shrink-0 basis-[38px]"
+              >
+                About
+              </div>
+              <div class="basis-10"></div>
+              <div
+                class="flex justify-end font-normal text-[14px] leading-normal font-Roboto text-white text-right tracking-[0.04px] shrink-0 basis-[52px]"
+              >
+                Register
+              </div>
+              <div class="basis-10"></div>
+              <div
+                class="flex justify-end font-normal text-[14px] leading-normal font-Roboto text-white text-right tracking-[0.04px] shrink-0 basis-[35px]"
+              >
+                Login
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+  
+      <section class="grid relative min-h-[1340px]">
+        <div class="xxl-xxxl:aspect-[auto] w-[894px] flex flex-col grow relative mt-[75px] mx-auto mb-[101px]">
+          <h1
+            class="xxl-xxxl:w-fit xxl-xxxl:aspect-[auto] flex items-center justify-center font-normal text-[48px] leading-[1.2] font-Roboto text-[rgb(33,37,41)] text-center w-[394px] mr-[245px] ml-[255px]"
+          >
+            Siste blogginnlegg
+          </h1>
+          <div class="basis-[97px]"></div>
+          <div class="sm:max-w-[894px] sm:aspect-[auto] w-[894px] flex flex-col gap-y-[49px] relative">
+            <div class="xl:basis-[480px] grid">
+              <div class="sm:aspect-[auto] w-[894px] flex items-center grow relative max-w-[894px]">
+                <div class="w-[404px] flex flex-col gap-y-[11px] shrink-0 basis-auto relative">
+                  <img
+                    class="outline outline-black outline-1 outline-offset-[-1px] w-full object-cover min-h-[306px]"
+                    src="/assets/3f852b4429873fc34cf34958864c2bc5.png"
+                    alt="alt text"
+                  />
+                  <div class="xxl-xxxl:w-fit xxl-xxxl:aspect-[auto] flex flex-col relative mr-2.5">
+                    <h4
+                      class="text-[19px] leading-[1.23] font-[&#x27;Segoe_UI&#x27;,Helvetica,Arial,serif] text-[rgb(51,51,51)]"
+                    >
+                      Blog headline
+                    </h4>
+                    <h5 class="text-base font-[&#x27;Segoe_UI&#x27;,Helvetica,Arial,serif] text-[rgb(33,37,41)] mt-[3px]">
+                      Nam et pulvinar neque, in dapibus dolor. Donec pellentesque feugiat blandit. Duis vulputate metus
+                      vitae augue iaculis mattis. Sed non lorem vulputate, accumsan libero id, jaod iajsjd jjajsd jaidsk0a
+                      s...
+                    </h5>
+                    <h5 class="text-base font-[&#x27;Segoe_UI&#x27;,Helvetica,Arial,serif] text-[rgb(13,110,253)] mt-4">
+                      Fortsett å lese
+                    </h5>
+                  </div>
+                </div>
+                <div class="basis-[86px]"></div>
+                <div class="sm:aspect-[auto] w-[404px] flex flex-col gap-y-[11px] shrink-0 basis-auto relative">
+                  <img
+                    class="outline outline-black outline-1 outline-offset-[-1px] w-full object-cover min-h-[306px]"
+                    src="/assets/3f852b4429873fc34cf34958864c2bc5.png"
+                    alt="alt text"
+                  />
+                  <div class="xxl-xxxl:w-fit xxl-xxxl:aspect-[auto] flex flex-col relative mr-2.5">
+                    <h4
+                      class="text-[19px] leading-[1.23] font-[&#x27;Segoe_UI&#x27;,Helvetica,Arial,serif] text-[rgb(51,51,51)]"
+                    >
+                      Blog headline
+                    </h4>
+                    <h5 class="text-base font-[&#x27;Segoe_UI&#x27;,Helvetica,Arial,serif] text-[rgb(33,37,41)] mt-[3px]">
+                      Nam et pulvinar neque, in dapibus dolor. Donec pellentesque feugiat blandit. Duis vulputate metus
+                      vitae augue iaculis mattis. Sed non lorem vulputate, accumsan libero id, jaod iajsjd jjajsd jaidsk0a
+                      s...
+                    </h5>
+                    <h5 class="text-base font-[&#x27;Segoe_UI&#x27;,Helvetica,Arial,serif] text-[rgb(13,110,253)] mt-4">
+                      Fortsett å lese
+                    </h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="grid">
+              <div class="sm:aspect-[auto] w-[894px] flex items-center grow relative max-w-[894px]">
+                <div class="w-[404px] flex flex-col gap-y-[11px] shrink-0 basis-auto relative">
+                  <img
+                    class="outline outline-black outline-1 outline-offset-[-1px] w-full object-cover min-h-[306px]"
+                    src="/assets/3f852b4429873fc34cf34958864c2bc5.png"
+                    alt="alt text"
+                  />
+                  <div class="flex flex-col relative mr-2.5">
+                    <h4
+                      class="text-[19px] leading-[1.23] font-[&#x27;Segoe_UI&#x27;,Helvetica,Arial,serif] text-[rgb(51,51,51)]"
+                    >
+                      Blog headline
+                    </h4>
+                    <h5 class="text-base font-[&#x27;Segoe_UI&#x27;,Helvetica,Arial,serif] text-[rgb(33,37,41)] mt-[3px]">
+                      Nam et pulvinar neque, in dapibus dolor. Donec pellentesque feugiat blandit. Duis vulputate metus
+                      vitae augue iaculis mattis. Sed non lorem vulputate, accumsan libero id, jaod iajsjd jjajsd jaidsk0a
+                      s...
+                    </h5>
+                    <h5 class="text-base font-[&#x27;Segoe_UI&#x27;,Helvetica,Arial,serif] text-[rgb(13,110,253)] mt-4">
+                      Fortsett å lese
+                    </h5>
+                  </div>
+                </div>
+                <div class="basis-[86px]"></div>
+                <div class="w-[404px] flex flex-col gap-y-[11px] shrink-0 basis-auto relative">
+                  <img
+                    class="outline outline-black outline-1 outline-offset-[-1px] w-full object-cover min-h-[306px]"
+                    src="/assets/3f852b4429873fc34cf34958864c2bc5.png"
+                    alt="alt text"
+                  />
+                  <div class="flex flex-col relative mr-2.5">
+                    <h4
+                      class="text-[19px] leading-[1.23] font-[&#x27;Segoe_UI&#x27;,Helvetica,Arial,serif] text-[rgb(51,51,51)]"
+                    >
+                      Blog headline
+                    </h4>
+                    <h5 class="text-base font-[&#x27;Segoe_UI&#x27;,Helvetica,Arial,serif] text-[rgb(33,37,41)] mt-[3px]">
+                      Nam et pulvinar neque, in dapibus dolor. Donec pellentesque feugiat blandit. Duis vulputate metus
+                      vitae augue iaculis mattis. Sed non lorem vulputate, accumsan libero id, jaod iajsjd jjajsd jaidsk0a
+                      s...
+                    </h5>
+                    <h5 class="text-base font-[&#x27;Segoe_UI&#x27;,Helvetica,Arial,serif] text-[rgb(13,110,253)] mt-4">
+                      Fortsett å lese
+                    </h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+  
+      <section class="grid relative min-h-[259px]">
+        <div class="grid bg-[rgb(0,157,147)] overflow-hidden grow relative mt-[168px] mb-px">
+          <div
+            class="flex items-center justify-center text-[14px] leading-[1.59] font-[&#x27;Segoe_UI&#x27;,Helvetica,Arial,serif] text-white text-center w-[247px] h-[23px] grow mt-[34px] mx-auto mb-[33px]"
+          >
+            Nettside bygget av Vetle Hagen Hansen
+          </div>
+        </div>
+      </section>
+    </main>
+  </template>
+  
+  <style scoped lang="scss">
+  @import 'BlogPage.scss';
+  </style>
+  
