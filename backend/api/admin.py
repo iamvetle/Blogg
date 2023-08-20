006d388312bdb84b6132a0feae6af6a58e918f66
iamvetle <vetle122@gmail.com>
2023-08-20 21:25:21 +0200

    ACTUALLY fixed it so i can create a user in admin panel BUT had to delete all data

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -7,14 +7,50 @@ from django.contrib.auth.admin import UserAdmin
 # Customize
 
 # Inlines ->
+class CommentInline(admin.TabularInline):
+    model = Comment
+    fields = ["title", "content", "post", "author", "date_published"]
+    readonly_fields = ["date_published"]
+    extra = 0
+    can_delete = False
+    show_change_link = True
+    
+    def has_change_permission(self, request, obj=None):
+        """
+        Only allow changing comments if obj is None (which means it's a new comment)
+        """
+        return not obj
+
+class PostInline(admin.TabularInline):
+    model = Post
+    fields = ["title", "content", "date_published", "last_modified"]
+    readonly_fields = ["date_published", "last_modified"]
+    extra = 0
+    can_delete = False
+    show_change_link = True
+    def has_change_permission(self, request, obj=None):
+        """
+        Only allow changing comments if obj is None (which means it's a new comment)
+        """
+        return not obj
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CommentInline]
     
 # Admin models ->
 
 
 # Forms ? ->
  
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
 
     
 # Register ->
     
 admin.site.register(CustomUser, UserAdmin)
+
+
+admin.site.register(Post, PostAdmin)
+
+admin.site.register(Comment)
