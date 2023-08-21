9481743fdb35626bda0cf0997218adfdbb86f830
iamvetle <vetle122@gmail.com>
2023-08-21 22:35:26 +0200

    .

---
+++ frontend/pages/register.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+//@ts-nocheck
+import axios from 'axios'
+
+const baseURL = "http://localhost:8888/api/register/"
+const submitted = ref(false)
+
+const submitForm = (hello) => {
+  console.log(hello)
+  const hey = hello
+
+  axios.post(baseURL, hey)
+  .then((response) => {
+    console.log(response.data)
+  })
+  submitted.value = true
+}
+
+</script>
+
+<template>
+  <FormKit
+    type="form"
+    id="register_form"
+    :form-class="submitted ? 'hide' : 'show'"
+    submit-label="Create account"
+    @submit="submitForm"
+    :actions="false"
+    #default="{ value }"
+  >
+    <h1>Registration form</h1>
+    <hr />
+    <FormKit
+      type="text"
+      name="username"
+      label="Username"
+      placeholder="example"
+      help="Write a username"
+      validation="required"
+    />
+    <FormKit
+      type="text"
+      name="first_name"
+      label="First name"
+      placeholder="Jane"
+      help="Write your first name"
+      validation="required"
+    />
+    <FormKit
+      type="text"
+      name="last_name"
+      label="Last name"
+      placeholder="Doe"
+      help="Write your last name"
+      validation="required"
+    />
+    <FormKit
+      type="text"
+      name="email"
+      label="Email"
+      placeholder="jane@example.com"
+      help="Write your email address"
+      validation="required|email"
+    />
+    <div class="double">
+      <FormKit
+        type="password"
+        name="password"
+        label="Password"
+        validation="required|length:6|matches:/[^a-zA-Z]/"
+        :validation-messages="{
+          matches: 'Please include at least one symbol',
+        }"
+        placeholder="Your password"
+        help="Choose a password"
+      />
+      <FormKit
+        type="password"
+        name="password_confirm"
+        label="Confirm password"
+        placeholder="Confirm password"
+        validation="required|confirm"
+        help="Confirm your password"
+      />
+    </div>
+
+    <FormKit
+      type="submit"
+      label="Register"
+    />
+    <pre wrap>{{ value }}</pre>
+  </FormKit>
+  <div v-if="submitted">
+    <h2>Submission successful!</h2>
+  </div>
+</template>
