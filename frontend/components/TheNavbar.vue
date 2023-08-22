c3d5ec8979e8cd8924fa861b0bfb65cec1f0b297
iamvetle <vetle122@gmail.com>
2023-08-22 22:06:51 +0200

    .

--- frontend/components/TheNavbar.vue
+++ frontend/components/TheNavbar.vue
@@ -1,11 +1,11 @@
 <template>
     <div id="navbar-wrapper"> <!-- Navbar -->
         <div class="w-full h-[70px] bg-[#DEBA6F] justify-between flex items-center ">
-            <logo>
+            <div id="logo">
                 <nuxt-link to="/">
                     <img src="~/assets/noimage.jpg" alt="The Blog - logo" class="w-auto h-[60px] ms-7">
                 </nuxt-link>
-            </logo>
+            </div>
             <ul class="flex space-x-5 me-7">
                 <nuxt-link to="/"><li class="hover:text-red-300">Home</li></nuxt-link>
                 <nuxt-link to="/registrer/"><li class="">Registrer</li></nuxt-link>
--- frontend/components/home/HomeTheNavbar.vue
+++ frontend/components/home/HomeTheNavbar.vue
@@ -1,12 +1,20 @@
 <template>
-    <div id="navbar" class="w-full text-white py-2 px-1 h-[50px] bg-[#823038] flex justify-end items-center">
+    <div id="navbar" class="w-full text-white py-2 px-1 h-[50px] bg-[#823038] flex justify-between items-center">
         <div v-if="isAuthenticated">
+            <div id="logo" class="flex">
+                <nuxt-link to="/">
+                    <img src="~/assets/noimage.jpg" alt="The Blog - logo" class="w-auto h-[60px] ms-7">
+                </nuxt-link>
+            </div>
             <ul class="flex text-sm">
                 <nuxt-link to="/kontakt/">
                     <li class="me-7 hover:text-yellow-300">Kontakt</li>
                 </nuxt-link>
                 <nuxt-link to="/om-oss/">
                     <li class="me-7 hover:text-yellow-300">Om oss</li>
+                </nuxt-link>
+                <nuxt-link to="/newpost/">
+                    <li class="me-7 hover:text-yellow-300">Nytt innlegg</li>
                 </nuxt-link>
                 <nuxt-link to="/myuser/">
                     <li class="me-7 hover:text-yellow-300">Min Side</li>
@@ -18,6 +26,11 @@
         </div>
         
         <div v-else>
+            <div id="logo" class="flex">
+                <nuxt-link to="/">
+                    <img src="~/assets/noimage.jpg" alt="The Blog - logo" class="w-auto h-auto ms-7">
+                </nuxt-link>
+            </div>
             <ul class="flex text-sm">
                 <nuxt-link to="/kontakt/">
                     <li class="me-7 hover:text-yellow-300">Kontakt</li>
