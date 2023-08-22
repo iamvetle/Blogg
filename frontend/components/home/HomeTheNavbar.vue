de1ea7a47cec8ef87fb358c03530a959b92cb60d
iamvetle <vetle122@gmail.com>
2023-08-22 20:56:23 +0200

    .

---
+++ frontend/components/home/HomeTheNavbar.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id="navbar" class="w-full text-white py-2 px-1 h-[50px] bg-[#823038] flex justify-end items-center">
+        <div v-if="isAuthenticated">
+            <ul class="flex text-sm">
+                <nuxt-link to="/kontakt/">
+                    <li class="me-7 hover:text-yellow-300">Kontakt</li>
+                </nuxt-link>
+                <nuxt-link to="/om-oss/">
+                    <li class="me-7 hover:text-yellow-300">Om oss</li>
+                </nuxt-link>
+                <nuxt-link to="/myuser/">
+                    <li class="me-7 hover:text-yellow-300">Min Side</li>
+                </nuxt-link>
+                <nuxt-link to="/loggut/">
+                    <li class="me-7 hover:text-yellow-300">Logg ut</li>
+                </nuxt-link>
+            </ul>
+        </div>
+        
+        <div v-else>
+            <ul class="flex text-sm">
+                <nuxt-link to="/kontakt/">
+                    <li class="me-7 hover:text-yellow-300">Kontakt</li>
+                </nuxt-link>
+                <nuxt-link to="/om-oss/">
+                    <li class="me-7 hover:text-yellow-300">Om oss</li>
+                </nuxt-link>
+                <nuxt-link to="/registrer/">
+                    <li class="me-7 hover:text-yellow-300">Registrer</li>
+                </nuxt-link>
+                <nuxt-link to="/login/">
+                    <li class="me-7 hover:text-yellow-300">Logg inn</li>
+                </nuxt-link>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts"> // Used by mainpage.vue - for feed
+
+const isAuthenticated = ref(false)
+
+function checkToken() {
+    if (localStorage.getItem("token") != null) {
+        isAuthenticated.value = true
+    }
+}
+
+onMounted(checkToken)
+
+</script>
+
+<style scoped>
+
+</style>
+
---
+++ frontend/pages/loggut.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div v-if="removed">
+            <p>You are now logged out.</p>
+        </div>
+        <div v-else>
+            <p>Logged out.</p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+const removed = ref(false)
+
+function logoutFunction() {
+    if (localStorage.getItem("token") != null) {
+        localStorage.removeItem("token")
+        removed.value = true
+    }
+}
+
+onMounted(logoutFunction)
+
+</script>
+
+<style scoped>
+
+</style>
