79625d75256ea7f0d743db271991f4edf0fc3a8c
iamvetle <vetle122@gmail.com>
2023-08-23 23:22:48 +0200

    .

---
+++ frontend/server/api/allposts.ts
@@ -0,0 +1,14 @@
+// /server/api/feed.js
+import axios from 'axios';
+
+export default async (req, res) => {
+    const baseURL = "http://localhost:8888/api/feed/";
+    try {
+        const response = await axios.get(baseURL);
+        console.log("All posts retrieved:", response.data);
+        res.json(response.data);
+    } catch (error) {
+        console.error("Error fetching all posts:", error);
+        res.status(500).json({ error: "Error fetching posts" });
+    }
+}???
