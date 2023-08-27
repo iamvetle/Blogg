d3291b16ac830f4f9548ea4eddd6d35367309866
iamvetle <vetle122@gmail.com>
2023-08-27 20:27:49 +0200

    Fikset problemet med 'useFetch'. Var flere console.log feil foran som jeg hadde lagt til, som ødela.

--- frontend/store/posts.ts
+++ frontend/store/posts.ts
@@ -34,15 +34,11 @@ interface AccountType {
 async function fetchAllPosts() {
 	try {
 		const {data: response} = await useFetch<PostType[]>("http://localhost:8888/api/feed/")
-
-		console.log(response.data.value)
-		console.log(toRaw(response.value))
-		console.log(response.value)
-		console.log("Success: fetched all posts", response.data.value)
 		posts.value = toRaw(response.value)
+		console.log("Success: fetched all posts", toRaw(response.value))
 
 	} catch {
-		console.log("Error: failed to fetch tasks");
+		console.log("Error: failed to fetch tasks")
 	}
 }
 async function loginPost(formData) {
