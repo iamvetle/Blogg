9253bc5389c5b21a5dd13b86bc78fb4b3cda6fc8
iamvetle <vetle122@gmail.com>
2023-08-28 21:08:20 +0200

    .

---
+++ frontend/composables/createNewPost.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+export const createNewPost = async (url:string, formData:object) => {
+    const token = localStorage.getItem("token")
+
+    try {
+        const response = await axios.post(url, formData, { 
+            headers: {
+                'Authorization': `Token ${token}`  
+            }
+        })
+        console.log("OK: a new post was created")
+    } catch {
+        console.log("FAILED: no new post was created")
+    }
+}
