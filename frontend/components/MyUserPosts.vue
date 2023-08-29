3f11beed60d219f6bab3b9d904e934c86e301506
iamvetle <vetle122@gmail.com>
2023-08-29 22:10:35 +0200

    Endret navn på 'components' til noe mer passende.

---
+++ frontend/components/MyUserPosts.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="mb-6">
+        <div class="flex justify-between"> 
+            <span class="text-gray-600 font-bold">{{ postProp.title }}</span>
+            <span class="flex">
+                <span class="text-gray-600 mr-2">posted: </span>
+                <span class="text-gray-600">{{ postProp.date_published }}</span>
+            </span>
+        </div>
+        <p class="mt-2">
+            {{ postProp.content }}
+        </p>
+    </div>
+</template>
+
+<script setup lang="ts"> // For the posts in the 'min-side'
+
+const { postProp, accountProp } = defineProps(["postProp", "accountProp"])
+
+</script>
+
+
+<style scoped>
+</style>
