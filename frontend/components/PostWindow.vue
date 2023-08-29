3873745a9037d913ac005ed68fe32d8115406a98
iamvetle <vetle122@gmail.com>
2023-08-29 22:18:35 +0200

    Byttet profil bilde

--- frontend/components/PostWindow.vue
+++ frontend/components/PostWindow.vue
@@ -1,10 +1,6 @@
 <template>
         <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"> <!-- Type one -->
-            <div class="flex justify-between items-center mb-5 text-gray-500">
-                <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                    <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
-                    Tutorial
-                </span>
+            <div class="flex justify-end items-center mb-5 text-gray-500">
                 <span class="text-sm">{{ postDetail.date_published }}</span>
             </div>
             <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">{{ postDetail.title }}</a></h2>
@@ -13,7 +9,7 @@
             </p>
             <div class="flex justify-between items-center">
                 <div class="flex items-center space-x-4">
-                    <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" />
+                    <img class="w-7 h-7 rounded-full" src="~assets/no.png" alt="temporary universal profile-picture" />
                     <span class="font-medium dark:text-white">
                         {{ author_full_name }}
                     </span>
