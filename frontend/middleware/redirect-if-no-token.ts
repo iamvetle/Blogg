3f526af793caa144d6237612c41bb1cd66880362
iamvetle <105447771+iamvetle@users.noreply.github.com>
2023-08-29 12:08:41 +0200

    Update redirect-if-no-token.ts

--- frontend/middleware/redirect-if-no-token.ts
+++ frontend/middleware/redirect-if-no-token.ts
@@ -1,11 +1,7 @@
-import path from "path"
-
 export default defineNuxtRouteMiddleware((to) => {
     if (process.client) {
         const token = localStorage.getItem("token")
-        console.log("first part")
-        if (token === null) {
-            console.log("second part")
+        if (token == null) {
             return navigateTo("/")
         }
     }
