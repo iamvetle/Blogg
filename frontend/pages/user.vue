e07cf09285f0b75236298c4389f7737861aaeebf
iamvetle <vetle122@gmail.com>
2023-08-30 21:24:48 +0200

    .

---
+++ frontend/pages/user.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="site-wrapper">
+        
+        <div id="navbar">
+
+        </div>
+        
+        <div class="w-9/12 mx-auto">
+
+            <div class="grids cols-12 ">
+            
+                <div class="col-start-1 col-span-8">
+                
+                    <div id="header">
+
+                    </div>
+                    <div id="heading">
+
+                    </div>
+
+                    <div id="userlinks">
+
+                    </div>
+                    <div id="main">
+                        
+                        <PostArticle />
+                    
+                    </div>
+                    
+                </div>
+            
+                <div id="sidebar" class="col-end-7 col-span-4">
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
