00525698d3f3ecd0e3a9ce12820e44e6b75ecfa6
iamvetle <vetle122@gmail.com>
2023-08-31 17:34:04 +0200

    Lagt til 'fetchAllPosts' for å teste om '/user' får innleggene. Det gjør den. Kommet en god del på siden

--- frontend/components/PostArticle.vue
+++ frontend/components/PostArticle.vue
@@ -2,18 +2,20 @@
     <div class="vt-artice grid grid-cols-12 gap-4 border border-green-500">
 
         <div class="col-span-9 border-2 border-red-300">
-            <span class="text-xs flex py-2"><p class="me-1 prose-xs">Published by</p><p class="prose-xs font-bold me-2">username</p><p class="font-extralight prose-xs">- 1 day ago</p></span>
-            <h3 class="font-bold">Article name</h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, pariatur libero vitae dicta sint officia aliquid laborum dolor tempora ad possimus beatae delectus ipsam saepe rem iure illo architecto unde?</p>
+            <span class="text-xs flex py-2"><p class="me-1 prose-xs">Published by</p><p class="prose-xs font-bold me-2">{{ postProp.author.username }}</p><p class="font-extralight prose-xs">- 1 day ago</p></span>
+            <h3 class="font-bold">{{ postProp.title }}</h3>
+            <p>{{ postProp.content_snippet }}</p>
         </div>
 
         <div id="article-img" class="col-span-3 border-2 border-red-300">
-            <div class="h-[150px] bg-slate-100"></div>
+            <img src="~/assets/noimage.jpg" class="h-[150px] bg-slate-100">
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+
+const { author, postProp } = defineProps(["author", "postProp"])
 
 </script>
 
