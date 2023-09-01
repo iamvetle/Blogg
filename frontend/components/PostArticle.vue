e4b80a6698dbd586239c041afeeb7d6df700fbac
iamvetle <vetle122@gmail.com>
2023-09-01 14:26:30 +0200

    La til litt padding 'px-2' på div i PostArticle

--- frontend/components/PostArticle.vue
+++ frontend/components/PostArticle.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="vt-artice grid grid-cols-12 gap-4 border border-green-500">
 
-        <div class="col-span-9 border-2 border-red-300">
+        <div class="px-2 col-span-9 border-2 border-red-300">
             <span class="text-xs flex py-2"><p class="me-1 prose-xs">Published by</p><p class="prose-xs font-bold me-2">{{ postProp.author.username }}</p><p class="font-extralight prose-xs">- 1 day ago</p></span>
             <h3 class="font-bold">{{ postProp.title }}</h3>
             <p>{{ postProp.content_snippet }}</p>
+            <p class="pt-2 text-xs font-light hover:font-normal">Read more</p>
         </div>
 
         <div id="article-img" class="col-span-3 border-2 border-red-300">
