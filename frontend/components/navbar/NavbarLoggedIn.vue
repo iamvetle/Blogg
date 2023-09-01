0f2086991f3e65e01d85e698d7bd1a2680e040c6
iamvetle <vetle122@gmail.com>
2023-09-01 23:23:19 +0200

    .

--- frontend/components/navbar/NavbarLoggedIn.vue
+++ frontend/components/navbar/NavbarLoggedIn.vue
@@ -20,6 +20,10 @@
             <li>
                 <nuxt-link to="/newpost" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">New post</nuxt-link>
             </li>
+
+            <li>
+                <nuxt-link to="/" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Følger</nuxt-link>
+            </li>
             <li>
                 <nuxt-link to="/min-side" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Min side</nuxt-link>
             </li>
