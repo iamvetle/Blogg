4ccbb5e64730672615e5b46ba767eb5cae80a258
iamvetle <vetle122@gmail.com>
2023-09-04 18:33:38 +0200

    .

---
+++ frontend/composables/followUser.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+export const followUser = async (url:string) => {
+    try {
+        const token = localStorage.getItem("token")
+        !token ? console.log("no token") : "" 
+ 
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${token}`
+        }
+
+        const response = await axios.get(url, { headers })
+		console.log("OK: fetched personal user account", response.data) // print to self
+        return response.data
+    } catch (error) {
+        console.log(error)
+        return null
+    }
+}
