c3e31cb243d87dd20d68c8f7ee49b1b95f873a7c
iamvetle <vetle122@gmail.com>
2023-09-04 12:53:01 +0200

    .

--- frontend/tests/components/Wait.test.ts
+++
@@ -1,8 +0,0 @@
-import Wait from '@/components/Wait.vue'
-import { mount } from '@vue/test-utils'
-import test
-
-test("Should work", () => {
-    const wrapper = mount(Wait)
-    expect(wrapper.text()).toContain("You need to have an account")
-})
