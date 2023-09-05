30ba11a9c4c667791ee086b947fa26a3b388df8c
iamvetle <vetle122@gmail.com>
2023-09-05 16:38:00 +0200

    Kan gå fra /user/brukernavnher til /post/[id]

--- frontend/components/PostArticle.vue
+++ frontend/components/PostArticle.vue
@@ -5,8 +5,10 @@
             <span class="text-xs flex py-2"><p class="me-1 prose-xs">Published by</p><p class="prose-xs font-bold me-2">{{ postProp.username }}</p><p class="font-extralight prose-xs">- {{ postProp.posts[0].date_published }}</p></span>
             <h3 class="font-bold">{{ postProp.posts[0].title }}</h3>
             <p>{{ postProp.posts[0].content_snippet }}</p>
-            <p class="pt-2 text-xs font-light hover:font-normal">Read more</p>
-        </div>
+            <nuxt-link :to=readpost>
+                <p class="pt-2 text-xs font-light hover:font-normal">Read more</p>
+            </nuxt-link>
+            </div>
 
         <div id="article-img" class="col-span-3 border-2 border-red-300">
             <img src="~/assets/noimage.jpg" class="h-[150px] bg-slate-100">
@@ -17,6 +19,8 @@
 <script setup lang="ts">
 
 const { postProp } = defineProps(["postProp"])
+
+const readpost = `/post/${postProp.posts[0].id}`
 
 </script>
 
