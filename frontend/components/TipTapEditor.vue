6f4e6e8e259a495c580e4b99c8bf880288cdcdc1
iamvetle <vetle122@gmail.com>
2023-09-07 23:11:49 +0200

    har installert tiny, men kommer ikke til å bruke det

---
+++ frontend/components/TipTapEditor.vue
@@ -0,0 +1,22 @@
+<template>
+    <EditorContent :editor="editor" />
+</template>
+  
+<script setup lang=ts>
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
+const editor = useEditor({
+	content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+	extensions: [
+		StarterKit,
+
+	],
+	editorProps: {
+    attributes: {
+      class: 'prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none',
+    },
+  },
+})
+
+</script>
