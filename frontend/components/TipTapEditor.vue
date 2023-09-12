1f6d2e634f49b8eeb28508e004c264dbce509ac7
iamvetle <vetle122@gmail.com>
2023-09-12 20:46:46 +0200

    tiptapeditor fungerer greit, jeg velger å bruke denne og så hermer jeg etter medium. jeg fikk til slutt til å laste opp image på client siden

--- frontend/components/TipTapEditor.vue
+++ frontend/components/TipTapEditor.vue
@@ -1,15 +1,74 @@
 <template>
-    <EditorContent :editor="editor" />
+    <div id="editor-container">
+      <div id="editor-area">
+
+        <floating-menu :editor="editor" :tippy-options="{ duration: 100 }" v-if="editor">
+          <button @click="addImage">
+          set image
+        </button>
+        </floating-menu>
+        <bubble-menu
+        :editor="editor"
+        :tippy-options=" { duration: 100 }"
+        v-if="editor"
+        class="bg-black"
+        >
+        <button @click="editor.chain().focus().toggleBold().run()" :class="{ 'is-active': editor.isActive('bold')}"
+        class="mx-1 border text-white"
+        >Bold</button>
+
+        <button @click="editor.chain().focus().toggleItalic().run()" :class="{ 'is-active': editor.isActive('italic')}"
+        class="me-1 border text-white"
+        >Italic</button>
+
+        <button @click="editor.chain().focus().toggleHeading({ level: 3 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"        
+        class="me-1 border text-white"
+        >H3</button>
+
+        <button @click="editor.chain().focus().toggleHeading({ level: 4 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }"        
+        class="me-1 border text-white"
+        >H4</button>
+
+        <button @click="editor.chain().focus().toggleBlockquote().run()" :class="{ 'is-active': editor.isActive('blockquote') }"        
+        class="me-1 border text-white"
+        >Quote</button>
+        
+
+
+
+
+        </bubble-menu>
+        <editor-content :editor="editor" />
+      </div>
+    </div>
 </template>
   
-<script setup lang=ts>
-import { useEditor, EditorContent } from '@tiptap/vue-3'
-import StarterKit from '@tiptap/starter-kit'
+<script setup>
+import { useEditor, EditorContent, FloatingMenu } from '@tiptap/vue-3'
+import { BubbleMenu } from '@tiptap/vue-3';
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import Heading from '@tiptap/extension-heading'
+import Link from '@tiptap/extension-link'
+import Blockquote from '@tiptap/extension-blockquote'
+import Dropcursor from '@tiptap/extension-dropcursor'
+import Image from '@tiptap/extension-image'
 
 const editor = useEditor({
 	content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
 	extensions: [
-		StarterKit,
+    Link,
+    Heading.configure({
+      levels: [2,3,4]
+    }),
+    Blockquote,
+    Image,
+    Dropcursor,
+    Document,
+    Paragraph,
+    Text,
+    FloatingMenu
 
 	],
 	editorProps: {
@@ -19,4 +78,25 @@ const editor = useEditor({
   },
 })
 
+// medium (når hover):
+// ""BUBBLEMENU" bold, italic, link, h3, h4, blockquote, (private comment)
+// ""FLOATINGMENU" bold, italic, link, h3, h4, blockquote, (private comment)
+
+
+// methods 
+
+function addImage() {
+      const url = window.prompt('URL')
+
+      if (url) {
+        editor.value.chain().focus().setImage({ src: url }).run()
+      }
+    }
+
+
+
 </script>
+
+<style lang="scss">
+
+</style>
--- frontend/notes.md
+++
@@ -1,5 +0,0 @@
-do I need to use postcss?
-
-should I use
-eslint
-stylint
