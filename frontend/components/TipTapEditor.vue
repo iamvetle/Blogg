4a6311326cf09c34cf813ceb885d83dbe19e3c0b
iamvetle <vetle122@gmail.com>
2023-09-13 11:51:54 +0200

    forbedret editor enda mer - 'floating menu' som ligner på den på medium

--- frontend/components/TipTapEditor.vue
+++ frontend/components/TipTapEditor.vue
@@ -1,53 +1,96 @@
 <template>
-    <div id="editor-container">
-      <div id="editor-area">
-
-        <floating-menu :editor="editor" :tippy-options="{ duration: 100 }" v-if="editor"
-        class="floating-menu absolute z-10 bg-white shadow-lg rounded-lg p-2 space-x-2"
-
->
-        </floating-menu>
-        <bubble-menu
-        :editor="editor"
-        :tippy-options=" { duration: 100 }"
-        v-if="editor"
-        class="bg-black"
-        >
-        <button @click="editor.addCommands" :class="{ 'is-active': editor.isActive('bold')}"
-        class="mx-1 border text-white"
-        >Bold</button>
-
-        <button @click="editor.chain().focus().toggleItalic().run()" :class="{ 'is-active': editor.isActive('italic')}"
-        class="me-1 border text-white"
-        >Italic</button>
-
-        <button @click="setLink()" :class="{ 'is-active': editor.isActive('link')}"
-        class="me-1 border text-white"
-        >link</button>
-
-        <button @click="editor.chain().focus().toggleHeading({ level: 3 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"        
-        class="me-1 border text-white"
-        >H3</button>
-
-        <button @click="editor.chain().focus().toggleHeading({ level: 4 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }"        
-        class="me-1 border text-white"
-        >H4</button>
-
-        <button @click="editor.chain().focus().toggleBlockquote().run()" :class="{ 'is-active': editor.isActive('blockquote') }"        
-        class="me-1 border text-white"
-        >Quote</button>
-        
-
-
-
-
-        </bubble-menu>
-        <editor-content :editor="editor" />
-      </div>
-      <pre><code>{{ html }}</code></pre>
-    </div>
+	<div class="grid-cols-12 grid">
+		<div class="md:col-start-4 md:col-span-6 col-start-3 col-span-8 border" id="editor-container">
+		<div id="editor-area">
+			<floating-menu
+			:editor="editor"
+			:tippy-options=" { duration: 100 }"
+			v-if="editor"
+			class="flex-col items-center md:flex-row relative md:-left-[225px] -left-[80px] flex md:space-x-3 rounded-md border max-md:space-y-3 p-1 bg-plain shadow-md"
+			>
+				<button @click="editor.commands.toggleBold()" :class="{ 'is-active': editor.isActive('bold')}"
+				class="flex">
+				<img class="h-5 flex items-center" src="~/assets/icons/bold.svg">
+			</button>
+	
+				<button @click="editor.commands.toggleItalic()" :class="{ 'is-active': editor.isActive('italic') }"
+				class="flex">
+				<img class="h-5 flex items-center" src="~/assets/icons/italic.svg">
+			</button>
+	
+				<button @click="setLink()" :class="{ 'is-active': editor.isActive('link')}"
+				class="flex">
+				<img class="h-5 flex items-center" src="~/assets/icons/link.svg">
+			</button>
+	
+				<button @click="editor.chain().focus().toggleHeading({ level: 3 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
+				class="flex">
+				<img class="h-5 flex items-center" src="~/assets/icons/h-1.svg">
+			</button>
+	
+				<button @click="editor.chain().focus().toggleHeading({ level: 4 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }"
+				class="flex">
+				<img class="h-5 flex items-center" src="~/assets/icons/h-2.svg">
+			</button>
+	
+				<button @click="editor.chain().focus().toggleBlockquote().run()" :class="{ 'is-active': editor.isActive('blockquote') }"
+				class="">
+				<img class="h-5 flex items-center" src="~/assets/icons/quote-text.svg">
+			</button>
+	
+	
+	
+	
+	
+			</floating-menu>
+			<bubble-menu
+			:editor="editor"
+			:tippy-options=" { duration: 100 }"
+			v-if="editor"
+			class="space-x-3 flex items-center rounded-md border p-1 bg-plain shadow-md"
+			>
+				<button @click="editor.commands.toggleBold()" :class="{ 'is-active': editor.isActive('bold')}"
+				class="">
+				<img class="h-5 flex items-center" src="~/assets/icons/bold.svg">
+			</button>
+	
+				<button @click="editor.commands.toggleItalic()" :class="{ 'is-active': editor.isActive('italic') }"
+				class="">
+				<img class="h-5 flex items-center" src="~/assets/icons/italic.svg">
+			</button>
+	
+				<button @click="setLink()" :class="{ 'is-active': editor.isActive('link')}"
+				class="">
+				<img class="h-5 flex items-center" src="~/assets/icons/link.svg">
+			</button>
+	
+				<button @click="editor.chain().focus().toggleHeading({ level: 3 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
+				class="">
+				<img class="h-5 flex items-center" src="~/assets/icons/h-1.svg">
+			</button>
+	
+				<button @click="editor.chain().focus().toggleHeading({ level: 4 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }"
+				class="">
+				<img class="h-5 flex items-center" src="~/assets/icons/h-2.svg">
+			</button>
+	
+				<button @click="editor.chain().focus().toggleBlockquote().run()" :class="{ 'is-active': editor.isActive('blockquote') }"
+				class="">
+				<img class="h-5 flex items-center" src="~/assets/icons/quote-text.svg">
+			</button>
+	
+	
+	
+	
+	
+			</bubble-menu>
+			<editor-content :editor="editor" />
+		</div>
+		<!-- <pre><code>{{ html }}</code></pre> -->
+		</div>
+</div>
 </template>
-  
+
 <script setup>
 import { useEditor, EditorContent, FloatingMenu } from '@tiptap/vue-3'
 import { BubbleMenu } from '@tiptap/vue-3';
@@ -55,79 +98,77 @@ import Document from '@tiptap/extension-document'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
 import Heading from '@tiptap/extension-heading'
+import Italic from '@tiptap/extension-italic'
 import Link from '@tiptap/extension-link'
+import Bold from '@tiptap/extension-bold'
 import Blockquote from '@tiptap/extension-blockquote'
 import Dropcursor from '@tiptap/extension-dropcursor'
 import Image from '@tiptap/extension-image'
-import { mount } from '@vue/test-utils';
 
 const editor = useEditor({
-  "type": "doc",
+"type": "doc",
 	content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
 	extensions: [
-    Heading.configure({
-      levels: [2,3,4]
-    }),
-    Blockquote,
-    Image,
-    Dropcursor,
-    Document,
-    Paragraph,
-    Text,
-    FloatingMenu,
-    Link.configure({
-      autolink: true,
-      linkOnPaste: true,
-      openOnClick: true,
-      autolink: true,
-    })
+	Heading.configure({
+	levels: [2,3,4]
+	}),
+	Blockquote,
+	Image,
+	Dropcursor,
+	Document,
+	Paragraph,
+	Text,
+	Italic,
+	Link,
+	Bold,
+	FloatingMenu,
 
 	],
 	editorProps: {
-    attributes: {
-      class: 'prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none',
-    },
-  },
+	attributes: {
+	class: 'prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none',
+	},
+},
 })
 
 const html = ref(null)
 
 onMounted(() => {
-  editor.value.on("update", () => {
-  html.value = editor.value.getHTML()
+editor.value.on("update", () => {
+html.value = editor.value.getHTML()
 })
 
 })
 
 const setLink = () => {
-      const previousUrl = editor.value.getAttributes('link').href
-      const url = window.prompt('URL', previousUrl)
+	const previousUrl = editor.value.getAttributes('link').href
+	const url = window.prompt('URL', previousUrl)
 
-      // cancelled
-      if (url === null) {
-        return
-      }
+	// cancelled
+	if (url === null) {
+		return
+	}
 
-      // empty
-      if (url === '') {
-        editor.value
-          .chain()
-          .focus()
-          .extendMarkRange('link')
-          .unsetLink()
-          .run()
+	// empty
+	if (url === '') {
+		editor.value
+		.chain()
+		.focus()
+		.extendMarkRange('link')
+		.unsetLink()
+		.run()
 
-        return
-      }
+		return
+	}
 
-      // update link
-      editor.value
-        .chain()
-        .focus()
-        .extendMarkRange('link')
-        .setLink({ href: url })
-        .run()
-    }
+	// update link
+	editor.value
+		.chain()
+		.focus()
+		.extendMarkRange('link')
+		.setLink({ href: url })
+		.run()
+	}
 
 // medium (når hover):
 // ""BUBBLEMENU" bold, italic, link, h3, h4, blockquote, (private comment)
@@ -137,12 +178,12 @@ const setLink = () => {
 // methods 
 
 function addImage() {
-      const url = window.prompt('URL')
+	const url = window.prompt('URL')
 
-      if (url) {
-        editor.value.chain().focus().setImage({ src: url }).run()
-      }
-    }
+	if (url) {
+		editor.value.chain().focus().setImage({ src: url }).run()
+	}
+	}
 
 </script>
 
