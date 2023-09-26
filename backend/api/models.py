596705ad31e9c95d53dc73744221c6524bc21fc8
iamvetle <vetle122@gmail.com>
2023-09-26 22:54:29 +0200

    La til date_created i tillegg til date_published og også published på django post modelen

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,54 +1,71 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.conf import settings
 
-    # Signal to create or update the Profile model when the User model is created or updated
-
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, password, first_name, last_name, **extra_fields):
+    def create_user(
+        self, email, username, password, first_name, last_name, **extra_fields
+    ):
         if not email:
-            raise ValueError('The Email field must be set')
+            raise ValueError("The Email field must be set")
         if not username:
-            raise ValueError('The Username field must be set')
+            raise ValueError("The Username field must be set")
         if not password:
-            raise ValueError('The Password field must be set')
+            raise ValueError("The Password field must be set")
         if not first_name:
-            raise ValueError('The First name field must be set')
+            raise ValueError("The First name field must be set")
         if not last_name:
-            raise ValueError('The Last name field must be set')
+            raise ValueError("The Last name field must be set")
         email = self.normalize_email(email)
         user = self.model(
-            email=email, 
+            email=email,
             username=username,
             first_name=first_name,
-            last_name=last_name,  
-            **extra_fields)
+            last_name=last_name,
+            **extra_fields,
+        )
         user.set_password(password)
 
         user.save(using=self._db)
         return user
-    
-    def create_superuser(self, email, username, password, first_name, last_name, **extra_fields):
-        user = self.create_user(email, username, password, first_name, last_name, is_superuser = True, is_staff = True, **extra_fields)
-        
+
+    def create_superuser(
+        self, email, username, password, first_name, last_name, **extra_fields
+    ):
+        user = self.create_user(
+            email,
+            username,
+            password,
+            first_name,
+            last_name,
+            is_superuser=True,
+            is_staff=True,
+            **extra_fields,
+        )
+
         return user
 
+
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(max_length=15, unique=True, blank=False) # Required
-    email = models.EmailField(unique=True, max_length=100, blank=False) # Required
-    first_name = models.CharField(max_length=30, blank=False) # Required
-    last_name = models.CharField(max_length=30, blank=False) # Required
+    username = models.CharField(max_length=15, unique=True, blank=False)  # Required
+    email = models.EmailField(unique=True, max_length=100, blank=False)  # Required
+    first_name = models.CharField(max_length=30, blank=False)  # Required
+    last_name = models.CharField(max_length=30, blank=False)  # Required
     age = models.PositiveIntegerField(blank=True)
-    address = models.TextField(blank=True, default='')
+    address = models.TextField(blank=True, default="")
     last_online = models.DateTimeField(auto_now=True)
-    phone_number = models.CharField(max_length=20, blank=True, default='')
-    nickname = models.CharField(max_length=30, blank=True, default='')
+    phone_number = models.CharField(max_length=20, blank=True, default="")
+    nickname = models.CharField(max_length=30, blank=True, default="")
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    followers = models.ManyToManyField('self', related_name='following', symmetrical=False, blank=True)
+    followers = models.ManyToManyField(
+        "self", related_name="following", symmetrical=False, blank=True
+    )
 
     objects = CustomUserManager()
 
@@ -62,63 +79,53 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
-    
+
     def full_name(self):
-        return f'{self.first_name} {self.last_name}'
+        return f"{self.first_name} {self.last_name}"
 
     def short_name(self):
         return self.first_name
 
-# class Profile(models.model):
-#     user_account = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-#     bio = models.TextField(max_length=500, default="", blank=True)
-#     #profile_picture
-#     followers = models.ManyToManyField('self', related_name='following', symmetrical=False, blank=True)
-
-#     @receiver(post_save, sender=CustomUser)
-#     def create_or_update_user_profile(sender, instance, created, **kwargs):
-#         if created:
-#             Profile.objects.create(user=instance)
-#         instance.profile.save()
-
-    '''
+    """
     followers: A ManyToManyField with 'self' allows users to follow each other.
 
     related_name='following' allows you to access the users that a user is following.
     symmetrical=False ensures the relationship isn't automatically two-way (i.e., if User A follows User B, it doesn't mean User B follows User A).
-    blank=True allows for a user to have zero followers or following.'''
+    blank=True allows for a user to have zero followers or following."""
 
 
 class Post(models.Model):
-     title = models.CharField(max_length=100)
-     content = models.TextField()
-     date_published = models.DateField(auto_now_add=True)
-     last_modified = models.DateTimeField(auto_now=True)
-     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-     
-     def __str__(self):
-         return self.title
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=10000)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    date_published = models.DateTimeField(blank=True, null=True)
+    published = models.BooleanField(default=False)
+    
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField(max_length=500)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments"
+    )
     date_published = models.DateField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
-        return f'{self.title} (Post: {self.post})' 
-    
+        return f"{self.title} (Post: {self.post})"
+
     def full_comment(self):
-        return f'{self.title}\n{self.content}'
+        return f"{self.title}\n{self.content}"
 
-
-    
 
 # Categories - kanskje senere
-    # ?
+# ?
 # CustomUser
 # Birthday
 
