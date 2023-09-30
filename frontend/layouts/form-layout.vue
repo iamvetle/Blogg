c6259ce45907947431053ff070b148ebf3941ffc
iamvetle <vetle122@gmail.com>
2023-09-30 11:11:37 +0200

    .

---
+++ frontend/layouts/form-layout.vue
@@ -0,0 +1,37 @@
+<template>
+	<div
+	id="site-wrapper"
+	class="bg-surface flex flex-col min-h-screen"
+	>
+	<div
+		v-if="store.apiDownError === false"
+		class="flex-1 flex flex-col"
+	>
+		<ClientOnly>
+		<TheNavbarr />
+		</ClientOnly>
+		<div class="flex-1">
+		<slot />
+		</div>
+	</div>
+	<div
+		v-else
+		class="flex-1"
+	>
+		<ErrorAPIDown />
+	</div>
+	<div class="mt-auto">
+		<LazyTheFooter />
+	</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { useGeneralStore } from '~/store/generalStore';
+
+const store = useGeneralStore()
+
+console.log("'form-layout' layout is selected");
+</script>
+
+<style scoped></style>
