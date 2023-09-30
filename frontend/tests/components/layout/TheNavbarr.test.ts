0bbdd77d94b8e6e02ed39b28fb8057eebaccba25
iamvetle <vetle122@gmail.com>
2023-09-30 16:20:45 +0200

    fized those two tests

--- frontend/tests/components/layout/TheNavbarr.test.ts
+++ frontend/tests/components/layout/TheNavbarr.test.ts
@@ -1,21 +1,22 @@
 import { VueWrapper, mount } from '@vue/test-utils'
 import { createTestingPinia } from '@pinia/testing'
-import { useSearchStore } from '~/store/searchStore'
 import TheNavbarr from '~/components/layout/TheNavbarr.vue'
+import { useGeneralStore } from '~/store/generalStore'
 
 describe("thenavvbarr testing", () => {
     let wrapper:VueWrapper;
-    let store;
-    let pinia;
+    let store: any; 
+    let pinia: any;
 
     beforeEach(() => {
         pinia = createTestingPinia()
-        store = useSearchStore(pinia)
+        store = useGeneralStore(pinia)
         wrapper = mount(TheNavbarr, {
             global: {
                 plugins: [pinia],
             },
         })
+        store.isAuthenticated = true
     })
 
     test('exists', () => {
@@ -26,5 +27,19 @@ describe("thenavvbarr testing", () => {
         expect(wrapper.text()).toContain("Nytt innlegg")
         expect(wrapper.text()).toContain("Min profil")
 
+    })
+    test("does not render nytt innleg and min-side icons when not authenticated", () => {
+        wrapper.unmount()
+
+        store.isAuthenticated = false
+        
+        wrapper = mount(TheNavbarr, {
+            global: {
+                plugins: [pinia],
+            },
+        })
+
+        expect(wrapper.text()).not.toContain("Nytt innlegg")
+        expect(wrapper.text()).not.toContain("Min profil")
     })
 })
--- frontend/tests/components/layout/Wait.test.ts
+++ frontend/tests/components/layout/Wait.test.ts
@@ -2,6 +2,11 @@
 import Wait from '~/components/layout/Wait.vue'
 import { mount } from '@vue/test-utils'
 
+/** 'Stubs' the function. Basically "difussing" it */
+vi.stubGlobal("setPageLayout", () => {
+    return null
+})
+
 describe("testing TheWait component", () => {
     const wrapper = mount(Wait)
 
