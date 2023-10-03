b25a12c0526542778e2fdea15e4db4cd352b897b
iamvetle <vetle122@gmail.com>
2023-10-03 13:03:52 +0200

    .

---
+++ frontend/components/modules/Blogg/ListArticlesSidebar.vue
@@ -0,0 +1,51 @@
+<template>
+	<div id="aside" class="h-auto w-full mb-14">
+
+		<div v-if="userdata" data-test="myprofile" class="w-full">
+			<MyProfileCard :userProp="userdata" />
+		</div>
+
+		<hr class="mb-8">
+		
+		<div id="saved-posts" class="mx-auto w-full mb-8">
+			<h3 class=" text-[28px] mb-9">
+				Lagrede innlegg
+			</h3>
+			<div class="saved-article">
+				<ArticleSavedCard />
+			</div>
+			<p class="-mt-2 text-xs text-primary hover:text-primaryFixed">
+				Se alle
+			</p>
+		</div>
+
+		<hr class="mb-8">
+
+		<div id="following-card" class="mx-auto w-full">
+			<h3 class="text-[28px] mb-9">
+				Følger
+			</h3>
+			<div id="following">
+				<Following />
+			</div>
+			<span class="text-xs text-primary hover:text-primaryFixed">Se alle</span>
+		</div>
+
+	</div>
+</template>
+
+<script setup lang="ts">
+
+const userdata = ref<null | PersonalUserType>(null)
+
+onBeforeMount(async () => {
+	const baseMyUserURL = "http://localhost:8888/api/min-side/"
+
+	userdata.value = await fetchPersonalUser(baseMyUserURL)
+	
+	console.log(userdata.value.data) // print to self
+})
+
+</script>
+
+<style scoped></style>
---
+++ frontend/tests/components/modules/Blogg/ListArticlesSidebar.test.ts
@@ -0,0 +1,53 @@
+import { mount } from "@vue/test-utils";
+
+import ListArticlesSidebar from "~/components/modules/Blogg/ListArticlesSidebar.vue";
+import MyProfileCard from "~/components/modules/MyUser/MyProfileCard.vue";
+import ArticleSavedCard from "~/components/modules/MyUser/ArticleSavedCard.vue";
+import Following from "~/components/modules/MyUser/Following.vue";
+
+// Mock fetchPersonalUser function
+const mockFetchPersonalUser = async (url: string) => {
+  return { data: 'mockedData' }
+}
+
+describe('ListArticlesSidebar testing', () => {
+
+  let wrapper
+
+  beforeAll(async () => {
+    // Mock the fetchPersonalUser function
+    const userdata = ref(null)
+    userdata.value = await mockFetchPersonalUser('http://localhost:8888/api/min-side/')
+    
+    wrapper = mount(ListArticlesSidebar, {
+      global: {
+        components: { MyProfileCard, ArticleSavedCard, Following },
+        mocks: { fetchPersonalUser: mockFetchPersonalUser, userdata }
+      }
+    })
+  })
+
+  it('renders the component', () => {
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('renders MyProfileCard when userdata is available', () => {
+    const myProfile = wrapper.find('[data-test="myprofile"]')
+    expect(myProfile.exists()).toBe(true)
+  })
+
+  it('passes userdata prop to MyProfileCard', () => {
+    const myProfileCard = wrapper.findComponent(MyProfileCard)
+    expect(myProfileCard.props('userProp')).toEqual({ data: 'mockedData' })
+  })
+
+  it('renders ArticleSavedCard', () => {
+    const articleSavedCard = wrapper.findComponent(ArticleSavedCard)
+    expect(articleSavedCard.exists()).toBe(true)
+  })
+
+  it('renders Following', () => {
+    const following = wrapper.findComponent(Following)
+    expect(following.exists()).toBe(true)
+  })
+})
