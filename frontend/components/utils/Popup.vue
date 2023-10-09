5c4f8d6d90c0801d336c58301e09c1180f5ed3d3
iamvetle <vetle122@gmail.com>
2023-10-09 21:54:02 +0200

    .

---
+++ frontend/components/utils/Popup.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="root" v-if="isOpen" class="relative">
+        <teleport to="#modal-root">
+            <div id="modal" class="fixed inset-0 flex items-center justify-center z-50">
+                <p>hello there</p>
+                <button @click="isOpen = false">Close</button>
+            </div>
+        </teleport>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+
+
+const props = withDefaults(defineProps<{
+    initial:boolean
+}>(), {
+    initial:false
+});
+
+
+const isOpen = ref(props.initial)
+
+</script>
+
+<style scoped>
+
+
+
+
+
+</style>
