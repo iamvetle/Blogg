f1c7b7992e8eb0d6033af4225126d561bf5f5283
iamvetle <vetle122@gmail.com>
2023-10-28 21:12:56 +0200

    .

---
+++ frontend/composables/getToken.ts
@@ -0,0 +1,20 @@
+/**
+ * Retrieves and returns the (login) token from localStorage, or null, if no token is found
+ * 
+ * The function will only try to get from localStorage if it is on client side, else it will just return null.
+ * 
+ * @returns - The token from localStorage, or null 
+ */
+export const getToken = (): string | null => {
+    if (process.client) {
+        const token = localStorage.getItem("token");
+
+        if(token) {
+            return token
+        } else {
+            return null
+        }
+    }
+    // console.log("Not on the client side (process.client)") // print to self
+    return null
+}
