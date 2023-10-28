5519bf73af298ab6b24e4ba7f875451ccf653f9d
iamvetle <vetle122@gmail.com>
2023-10-28 17:59:37 +0200

    .

---
+++ frontend/components/modules/Editor/EditorButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <span class="flex">
+        <button :class="{ 'is-active': props.isActive }" @click="$emit('button-click')">
+            <img :class="props.class" :src="props.icon" :alt="props?.alt">
+        </button>
+    </span>
+</template>
+
+<script setup lang="ts">
+
+const props = withDefaults(defineProps<{
+    class?:string,
+    icon:string,
+    alt?:string
+    iconImgClass?:string,
+    isActive:string,
+}>(), {
+    class:"h-5 flex items-center"
+})
+
+</script>
+
+<style scoped>
+
+</style>
