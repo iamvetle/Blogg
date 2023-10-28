54dc1f809090e273a4cfce0e605a6fc75f32e7b1
iamvetle <vetle122@gmail.com>
2023-10-28 21:59:46 +0200

    logged in user all posts now using get method

--- frontend/composables/crud/getLoggedInUserAllPostSnippets.ts
+++ frontend/composables/crud/getLoggedInUserAllPostSnippets.ts
@@ -1,30 +1,45 @@
-// async
+import { getToken } from '../getToken';
+import { usePostStore } from '~/store/postStore'
 
-import axios from 'axios' 
-import { usePostStore } from '~/store/postStore';
-
+/**
+ * Fetches all of the posts made by the logged in user
+ *  
+ * @param userURL - The url that the request is going to try to fetch from
+ *  
+ * @returns - The request response (.data, .status) or null
+ */
 export const getLoggedInUserAllPostSnippets = async () => {
+
   const postStore = usePostStore()
 
-  try {
-    const token = localStorage.getItem("token");
+  /**
+   * Fetches the token from local storage, or just returns null.
+   */
+  const token = getToken()
+
+  if (token) {
     const headers = {
       "Content-Type": "application/json",
       Authorization: `Token ${token}`,
     };
 
-    const response = await axios.get<LoggedInUserMultiplePostType>(postStore.baseLoggedInUserPostsURL, { headers });
+    const response = await getMethod(postStore.baseLoggedInUserPostsURL, headers)
 
-    if (response.data != null) {
-      console.log("OK: Personal posts fetched", response.data); // print to self
-      
+    if (response) {
+
       postStore.loggedInUserPosts = response.data;
-    
-    } else {
-      console.log("OBS! Fetching succsedded, but response(data) was:", response.data) // print to self
-    }
 
-  } catch (error) {
-    console.error("ERROR: An error occured while trying to fetch personal posts: ", error); // print to self
+      postStore.posts = response.data
+
+      /**
+			 * Has the option to return the response **directly** as well.
+			 * This can be usefull to check if the response was 200 OK, for instance.
+			 */
+      return response
+
+    } else {
+
+      return null
+    }
   }
-};
+}
