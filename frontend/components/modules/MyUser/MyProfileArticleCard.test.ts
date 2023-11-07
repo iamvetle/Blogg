b8b1925f8f3bb712de467046c8ddb1007144a2fd
iamvetle <vetle122@gmail.com>
2023-11-07 17:21:53 +0100

    .

---
+++ frontend/components/modules/MyUser/MyProfileArticleCard.test.ts
@@ -0,0 +1,152 @@
+import MyProfileArticleCard from './MyProfileArticleCard.vue';
+import { VueWrapper, shallowMount } from '@vue/test-utils';
+import { createTestingPinia } from '@pinia/testing';
+
+// import { useGeneralStore } from '~/store/generalStore';
+// import { usePostStore } from '~/store/postStore';
+// import { useLoggedInUserStore } from '~/store/loggedInUserStore';
+// import { usePaginationStore } from '~/store/paginationStore';
+
+let wrapper: VueWrapper;
+let pinia: any = createTestingPinia();
+
+// let generalStore; 
+// let postStore; 
+// let loggedInUserStore; 
+// let paginationStore; 
+
+const mockRedirectToPostPage = vi.fn()
+
+const factory = () => {
+    return shallowMount(MyProfileArticleCard, {
+        global: {
+            plugins: [pinia],
+            components: {},
+            mocks: {
+                redirectToPostPage:mockRedirectToPostPage
+            },
+            stubs: {},
+        },
+        props: {
+            post: {
+                title: "posttesttitle",
+                content_snippet: "<div><span><p>postcontentsnippet</p><p>poop poop</p></span></div>",
+                date_published: "02-10-2321",
+                author: {
+                    username: "testusername",
+                    first_name: "testfirstname",
+                    last_name: "testlastname"
+                },
+                tags: [
+                    {
+                        name: "tag1"
+                    },
+                    {
+                        name: "tag2"
+                    }
+                ],
+                categories: [
+                    {
+                        name: "category1"
+                    },
+                    {
+                        name: "category2"
+                    }
+                ],
+                images: [
+                    {
+                        name: "~/image/src1.jpg"
+                    },
+                    {
+                        name: "~/image/src2.jpg"
+                    }
+                ],
+                videos: [
+                    {
+                        name: "~/video/src1.mp4"
+                    },
+                    {
+                        name: "~/video/src2.mp4"
+                    }
+                ],
+                num_of_comments: 8
+            }
+        },
+        slots: {}
+    })
+};
+
+describe('', () => {
+
+    vi.stubGlobal('definePageMeta', () => {
+        return null
+    });
+
+    // vi.stubGlobal('useRoute', () => {
+    //     return {
+    //         params: {
+    //             id: 1
+    //         }
+    //     }
+    // });
+
+    beforeEach(() => {
+        // generalStore = useGeneralStore(pinia); 
+        // postStore = usePostStore(pinia); 
+        // loggedInUserStore = useLoggedInUserStore(pinia); 
+        // paginationStore = usePaginationStore(pinia); 
+
+    });
+
+    afterEach(() => {
+        if (wrapper) {
+            wrapper.unmount();
+        }
+    });
+
+    test('Should render post title', () => {
+        wrapper = factory()
+        expect(wrapper.text()).toContain("posttesttitle")
+    })
+    test('Should render post content (snippet)', () => {
+        wrapper = factory()
+        expect(wrapper.text()).toContain("postcontentsnippet")
+    })
+    test('Should render date published', () => {
+        wrapper = factory()
+        expect(wrapper.text()).toContain("02-10-2321")
+    })
+    test("Should NOT render content_snippet (v-html) as direct html")
+    wrapper = factory()
+
+    const content = wrapper.get("[data-test='content-html']")
+
+    expect(content.text()).not.toContain("<div><span><p>postcontentsnippet</p><p>poop poop</p></span></div>")
+    expect(content.html()).not.toContain("<div><span><p>postcontentsnippet</p><p>poop poop</p></span></div>")
+    expect(content.text()).toBe("postcontentsnippetpoop poop")
+});
+test('Should render tags', () => {
+    wrapper = factory()
+
+    expect(wrapper.text()).toContain("tag1")
+    expect(wrapper.text()).toContain("tag2")
+})
+
+test('Should have a "read more" field', () => {
+    wrapper = factory()
+
+    expect(wrapper.text()).toContain("Les mer")
+})
+
+test('The read more "button" should call a redirect function', async () => {
+    wrapper = factory()
+
+    const read = wrapper.get("[data-test='read']")
+
+    await read.trigger("click")
+
+    await wrapper.vm.$nextTick()
+
+    expect(mockRedirectToPostPage).toHaveBeenCalledOnce()
+    
+})
---
+++ frontend/components/modules/MyUser/MyProfileArticleCard.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="bg-onPrimary mb-6 px-5 py-5 border rounded-lg border-primary">
+        <div class="flex justify-between">
+            <span class="font-bold">{{ post.title }}</span>
+            <span class="flex">
+                <span class="mr-2">posted: </span>
+                <span class="">{{ post.date_published }}</span>
+            </span>
+        </div>
+        <div class="break-words">
+            <p data-test="content-html" class="mt-2" v-text="toPlainText(post.content_snippet)">
+            </p>
+        </div>
+        <div class="flex">
+            <span v-for="tag in post.tags" :key="tag.name">{{ tag.name }}</span>
+        </div>
+        <span @click="redirectToPostPage(post.id)" data-test="read">
+            Les mer
+        </span>
+    </div>
+</template>
+
+<script setup lang="ts">
+// For the posts in the 'minkonto'
+
+const { post } = defineProps(["post"]);
+console.log(post)
+
+/**
+ * Redirects the web client to the page of the post 
+ * @param post 
+ */
+const redirectToPostPage = (postId: SnippetPostSingleType) => {
+
+    return navigateTo(`/post/${postId}`)
+}
+
+
+/** 
+ * The function takes the html input and returns only the raw text
+ * @returns - the raw text of the html
+*/
+const toPlainText = (htmlContent: string) => {
+    const div = document.createElement('div')
+    div.innerHTML = htmlContent
+    const rawText = div.textContent || div.innerText
+    return rawText
+}
+
+
+
+
+
+</script>
+
+
+<style scoped></style>
