387a6a5572965522485028810676f55e2db0727a
iamvetle <vetle122@gmail.com>
2023-11-12 22:00:29 +0100

    .

---
+++ frontend/layouts/creating.vue
@@ -0,0 +1,31 @@
+<template >
+    <!-- Background color -->
+    <div id="site-wrapper">
+        <ClientOnly>
+            <TheNavbar />
+        </ClientOnly>
+        <main>
+            <div>
+                <slot />
+            </div>
+        </main>
+        <TheFooter />
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+#site-wrapper {
+    background-color: #F0F2F2;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+}
+
+main {
+    flex: 1;
+}
+</style>
