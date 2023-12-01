fe0736fe9190cb76bd641c17db2a9814c8dcb2b5
iamvetle <vetle122@gmail.com>
2023-12-01 21:35:38 +0100

    Added a red ring when a change that was not been saved is at bio input

--- frontend/components/modules/MyProfile/MyProfileBio.vue
+++ frontend/components/modules/MyProfile/MyProfileBio.vue
@@ -2,12 +2,12 @@
     <div class="bg-primary rounded-lg p-6">
         <div class="px-5 py-8 bg-onPrimary border-primary border rounded-md">
             <h2 class="text-xl font-bold mb-4">
-                Bio
+                My Bio
             </h2>
 
 
             <div data-test="bio_input_output" id="bio-text" class="break-words">
-                <InputTextarea maxlength="275" class="py-4 mb-1 border-none resize-none rounded-lg w-full
+                <InputTextarea maxlength="275" :class="borderAndRingClass" class="py-4 mb-1 border-none resize-none rounded-lg w-full
                 
                 hover:ring-primaryContainer
                 hover:ring-2
@@ -17,7 +17,9 @@
                 " v-model.trim="bioText" id="bio" placeholder="Write bio here" />
             </div>
             <div v-if="showSaveBioInputButton" data-test="submit_bio_input_change">
-                <BaseButton class="float-right rounded-lg mb-2 p-1 text-sm text-onPrimary hover:text-onPrimaryFixed bg-primary hover:bg-primaryFixed hover:shadow-sm"  text="Save change" type="submit" @click="handleBioChange"/>
+                <BaseButton
+                    class="float-right rounded-lg mb-2 p-1 text-sm text-onPrimary hover:text-onPrimaryFixed bg-primary hover:bg-primaryFixed hover:shadow-sm"
+                    text="Save change" type="submit" @click="handleBioChange" />
             </div>
         </div>
     </div>
@@ -31,6 +33,9 @@ const emit = defineEmits(["bioUpdate"])
 
 const loggedInUserStore = useLoggedInUserStore()
 const bioText = ref<any>("")
+
+/** Controls if the ring/border is shown at "default" element state (no focus, hover etc.) */
+const borderAndRingClass = ref("")
 
 /**
  * Places the initial bio value when component is loaded
@@ -50,12 +55,16 @@ onMounted(() => {
 const showSaveBioInputButton = computed(() => {
     /** There is not text input - the button is not shown */
     if (bioText.value === "") {
+        borderAndRingClass.value = ""
         return false
-    /** There is text input but it is the excact same as it initially was - the button is not shown*/
+        /** There is text input but it is the excact same as it initially was - the button is not shown*/
     } else if (bioText.value === loggedInUserStore.loggedInUserProfile.bio) {
+        borderAndRingClass.value = ""
         return false
-    /** Otherwise, the button is shown */
+        /** Otherwise, the button is shown */
     } else {
+        /** Makes a red ring around the bio input indicating attention */
+        borderAndRingClass.value = "ring-error ring-2"
         return true
     }
 })
