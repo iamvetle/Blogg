98662f8479b1abd14e1c1184e7853deb367957a0
iamvetle <vetle122@gmail.com>
2023-12-01 14:49:18 +0100

    .

---
+++ frontend/composables/crud/patchLoggedInUserBio.ts
@@ -0,0 +1,28 @@
+import { patchMethod } from "~/services/apiByCRUD";
+
+export const patchLoggedInUserBio = async (url: string, formData: object): Promise<object | null> => {
+    try {
+        const token = retrieveToken();
+
+        const headers = {
+            "Authorization": `Token ${token}`,
+        };
+
+
+        /** The actual POST fetch */
+        const response = await patchMethod(url, formData, headers)
+
+        /** Returns the data from the response */
+        if (response) {
+            return response.data
+        } else {
+            console.log("FAILED: something failed"); // print to self
+            return null
+        }
+
+    } catch (error) {
+        console.error("ERROR: An error occurred while trying to update/add bio to logged in user: ", error); // Logging for debugging
+        return null;
+    }
+
+};
