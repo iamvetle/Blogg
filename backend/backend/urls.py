ae0e6f8ebf52002c7a753e604e1404ccf920183e
iamvetle <vetle122@gmail.com>
2023-12-13 13:59:55 +0100

    Switched around on some views, serializers and such

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("api.urls")),
-    path('users/', include("users.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
 
