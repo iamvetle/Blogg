7f7fe16393fc273580f1939edd42a3ab08b9be67
iamvetle <vetle122@gmail.com>
2023-12-27 23:28:53 +0100

    s

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,84 +1,84 @@
 export default defineNuxtConfig({
-	devtools: {
-		enabled: true,
+								devtools: {
+																enabled: true,
 
-		timeline: {
-			enabled: true,
+																timeline: {
+																								enabled: true,
 
-		},
-		
-	},
-	debug:true,
-	sourcemap: {
-		server: true,
-		client: true
-	  },
-	  spaLoadingTemplate:"./app/spa-loading-template.html",
-	vite: {
-		vue: {
-			script: {
-				defineModel: true,
-			},
-		},
-	},
-	components: [
-		{
-			path: '~/components',
-			pathPrefix: false,
-		},
-	],
-	imports: {
-		dirs: ['./typescript', './store', './utils/editor', './composables/crud', './composables/editor', './constants'],
-		presets: [
-			// {
-			// 	from: "./store",
-			// 	imports: [
-			// 		{ name: "usePaginationStore", as: "usePaginationStore" },
-			// 		{ name: "useLoggedInUserStore", as: "useLoggedInUserStore" },
-			// 		{ name: "useGeneralStore", as: "useGeneralStore" },
-			// 		{ name: "usePostStore", as: "usePostStore" },
-			// 		{ name: "useSearchStore", as: "useSearchStore" },
-			// 		{ name: "useAuthStore", as: "useAuthStore" }
+																},
+																
+								},
+								debug:true,
+								sourcemap: {
+																server: true,
+																client: true
+												},
+												spaLoadingTemplate:"./app/spa-loading-template.html",
+								vite: {
+																vue: {
+																								script: {
+																																defineModel: true,
+																								},
+																},
+								},
+								components: [
+																{
+																								path: '~/components',
+																								pathPrefix: false,
+																},
+								],
+								imports: {
+																dirs: ['./typescript', './store', './utils/editor', './composables/crud', './composables/editor', './constants'],
+																presets: [
+																								// {
+																								// 	from: "./store",
+																								// 	imports: [
+																								// 		{ name: "usePaginationStore", as: "usePaginationStore" },
+																								// 		{ name: "useLoggedInUserStore", as: "useLoggedInUserStore" },
+																								// 		{ name: "useGeneralStore", as: "useGeneralStore" },
+																								// 		{ name: "usePostStore", as: "usePostStore" },
+																								// 		{ name: "useSearchStore", as: "useSearchStore" },
+																								// 		{ name: "useAuthStore", as: "useAuthStore" }
 
-			// 	]
-			// },
-			{
-				from: 'axios',
-				imports: [
-					{ name: "default", as: "axios" },
-					{ name: "AxiosResponse", as: 'AxiosResponse', type: true }
-				]
-			}
+																								// 	]
+																								// },
+																								{
+																																from: 'axios',
+																																imports: [
+																																								{ name: "default", as: "axios" },
+																																								{ name: "AxiosResponse", as: 'AxiosResponse', type: true }
+																																]
+																								}
 
-		]
+																]
 
-	},
+								},
 
-	modules: [
-		"@formkit/nuxt",
-		"@pinia/nuxt",
-		"@nuxt/ui",
-		"nuxt-icon",
-		"nuxt-vitest"
-	],
-	pinia: {
-		autoImports: ["defineStore", ["defineStore", "definePiniaStore"]],
-		// storesDirs: ['./store/**']
-	},
-	ui: {
-		global: true,
-		icons: ['mdi', 'simple-icons', 'solar'],
-	},
-	tailwindcss: {
-		cssPath: "~/assets/css/tailwind.css",
-		configPath: "tailwind.config",
-		exposeConfig: false,
-		exposeLevel: 2,
-		config: {
-		},
-		injectPosition: "first",
-		viewer: true,
+								modules: [
+																"@formkit/nuxt",
+																"@pinia/nuxt",
+																"@nuxt/ui",
+																"nuxt-icon",
+																"nuxt-vitest"
+								],
+								pinia: {
+																autoImports: ["defineStore", ["defineStore", "definePiniaStore"]],
+																// storesDirs: ['./store/**']
+								},
+								ui: {
+																global: true,
+																icons: ['mdi', 'simple-icons', 'solar'],
+								},
+								tailwindcss: {
+																cssPath: "~/assets/css/tailwind.css",
+																configPath: "tailwind.config",
+																exposeConfig: false,
+																exposeLevel: 2,
+																config: {
+																},
+																injectPosition: "first",
+																viewer: true,
 
-	},
-	ssr: false
+								},
+								ssr: false
 });
